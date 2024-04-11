205d690210486f5fa3bf479481c8323b7f771731
MariiaHutsuliak <hutsuliak.mariia@chnu.edu.ua>
2024-04-11 21:43:14 +0300

    Testing Queues and Stack

--- lab7/main.cpp
+++ lab7/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "SinglyLinkedList.h"
 #include "DoublyLinkedList.h"
+#include "Queue.h"
+#include "OutputRestrictedQueue.h"
+#include "Stack.h"
 using namespace std;
 int main() {
     /*
@@ -48,7 +51,7 @@ int main() {
     cout << list << endl;
      } catch (const exception &e) {
         cerr << "Exception: " << e.what() << endl;
-    } */
+    }
     try {
     DoublyLinkedList<int> list;
 
@@ -80,12 +83,38 @@ int main() {
     cout << "List data after inserting element at index 1:" << endl;
     cout << list << endl;
 
-    list.removeAtIndex(23456);
+    list.removeAtIndex(2);
 
     cout << "List data after removing element at index 2:" << endl;
     cout << list << endl;
         } catch (const exception &e) {
         cerr << "Exception: " << e.what() << endl;
+    } */
+
+
+    Queue<int> queue(5);
+    queue.Enqueue(1);
+    queue.Enqueue(2);
+    queue.Enqueue(3);
+    cout << "Front element: " << queue.Peek() << endl;
+    cout << "Dequeue: " << queue.Dequeue() << endl;
+    cout << "Front element after dequeue: " << queue.Peek() << endl;
+
+    Stack<string> stack;
+    stack.Push("Hello");
+    stack.Push("World");
+    cout << "Top element: " << stack.Peek() << endl;
+    cout << "Pop: " << stack.Pop() << endl;
+    cout << "Top element after pop: " << stack.Peek() << endl;
+
+    OutputRestrictedQueue<string> q;
+    q.enqueueFront("apple");
+    q.enqueueRear("banana");
+    q.enqueueRear("orange");
+    cout << "Dequeued from front: " << q.dequeueFront() << endl;
+    q.enqueueFront("grape");
+    while (!q.isEmpty()) {
+        cout << "Dequeued from front: " << q.dequeueFront() << endl;
     }
     return 0;
 }
