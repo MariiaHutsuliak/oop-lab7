c205e7c485aa5406f9c2a0046dc0041d19dd7296
MariiaHutsuliak <hutsuliak.mariia@chnu.edu.ua>
2024-04-11 21:42:36 +0300

    Add OutputRestrictedQueue

--- lab7/OutputRestrictedQueue.h
+++ lab7/OutputRestrictedQueue.h
@@ -1,8 +1,70 @@
-//
-// Created by user on 11.04.2024.
-//
-
 #ifndef LAB7_OUTPUTRESTRICTEDQUEUE_H
 #define LAB7_OUTPUTRESTRICTEDQUEUE_H
+#include <iostream>
+using namespace std;
+template <typename T>
+class OutputRestrictedQueue {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node* prev;
 
+        Node(const T& data) : data{data}, next{nullptr}, prev{nullptr} {}
+    };
+    Node* front;
+    Node* rear;
+
+public:
+    OutputRestrictedQueue() : front(nullptr), rear(nullptr) {}
+
+    ~OutputRestrictedQueue() {
+        while (!isEmpty()) {
+            dequeueFront();
+        }
+    }
+
+    void enqueueFront(const T& element) {
+        Node* newNode = new Node(element);
+        if (isEmpty()) {
+            front = rear = newNode;
+        } else {
+            newNode->next = front;
+            front->prev = newNode;
+            front = newNode;
+        }
+    }
+
+    void enqueueRear(const T& element) {
+        Node* newNode = new Node(element);
+        if (isEmpty()) {
+            front = rear = newNode;
+        } else {
+            rear->next = newNode;
+            newNode->prev = rear;
+            rear = newNode;
+        }
+    }
+
+    T dequeueFront() {
+        if (isEmpty()) {
+            cerr << "Queue is empty" << endl;
+            exit(1);
+        }
+        T elements = front->data;
+        Node* temp = front;
+        if (front == rear) {
+            front = rear = nullptr;
+        } else {
+            front = front->next;
+            front->prev = nullptr;
+        }
+        delete temp;
+        return elements;
+    }
+
+    bool isEmpty() {
+        return front == nullptr;
+    }
+};
 #endif //LAB7_OUTPUTRESTRICTEDQUEUE_H
