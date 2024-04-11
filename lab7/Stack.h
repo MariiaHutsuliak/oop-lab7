9d90b639bc61e473b18f9c8c8cb5ca92fe9fefff
MariiaHutsuliak <hutsuliak.mariia@chnu.edu.ua>
2024-04-11 21:41:47 +0300

    Add Stack

--- lab7/Stack.h
+++ lab7/Stack.h
@@ -1,8 +1,59 @@
-//
-// Created by user on 11.04.2024.
-//
-
 #ifndef LAB7_STACK_H
 #define LAB7_STACK_H
+#include <iostream>
+using namespace std;
+#include <stdexcept>
+template <typename T>
+class Stack {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(const T& value) : data{value}, next{nullptr} {}
+    };
+    Node* top;
+    size_t size;
+
+public:
+    Stack():top{nullptr} {}
+    ~Stack() {
+        while (!isEmpty()) {
+            Pop();
+        }
+    }
+
+    bool isEmpty() {
+        return top == nullptr;
+    }
+
+    T Peek() {
+        if (isEmpty()) {
+            cerr << "Stack is empty" << endl;
+            exit(1);
+        }
+        return top->data;
+    }
+
+    T Pop() {
+        if (isEmpty()) {
+            cerr << "Stack is empty" << endl;
+            exit(1);
+        }
+        T data = top->data;
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+        size--;
+        return data;
+    }
+
+    void Push(const T& element) {
+        Node* newNode = new Node(element);
+        newNode->next = top;
+        top = newNode;
+        size++;
+    }
+
+};
 
 #endif //LAB7_STACK_H
